99cb00ce5a764dfde839bfd7894e14ab08b3aaa6
anh.do <tuananh9h@gmail.com>
2024-08-05 10:43:19 +0700

    sass init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginSass from 'vite-plugin-sass-glob-import'
+import sassDts from 'vite-plugin-sass-dts'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles" as common;`,
+        importer(...args: [string, string]) {
+          if (args[0] !== '@/styles') {
+            return
+          }
+
+          return {
+            file: `${path.resolve(__dirname, './src/assets/styles')}`,
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(), vitePluginSass(), 
+    sassDts({
+      enabledMode: ['development', 'production'],
+      global: {
+        generate: true,
+        outputFilePath: path.resolve(__dirname, './src/style.d.ts'),
+      },
+      sourceDir: path.resolve(__dirname, './src'),
+      outputDir: path.resolve(__dirname, './dist'),
+    })],
 })
