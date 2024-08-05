43f27444861d32213ae6d7842ec3df0d25a9fe29
anh.do <tuananh9h@gmail.com>
2024-08-05 16:21:13 +0700

    npx tailwind init --ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,31 @@
 import type { Config } from 'tailwindcss'
+
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Poppins', 'sans-serif'],
-            },
-            colors: {
-                primary: '#1a1a1a',
-                secondary: '#101010',
-                dark: '#1e1e1e',
-                light: '#f5f5f5',
-            },
-            container: {
-                center: true,
-                padding: {
-                    DEFAULT: '1rem',
-                    sm: '3rem',
-                },
-            },
-            animation: {
-                'spin-slow': 'spin 40s linear infinite',
-            },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#1a1a1a",
+        secondary: "#101010",
+        dark: "#1e1e1e",
+        light: "#f5f5f5",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
         },
+      },
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+      },
     },
-    plugins: [],
-} satisfies Config;
+  },
+  plugins: [],
+} satisfies Config
+
