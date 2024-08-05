765ac086b01febe277dc201bef1ac63a8be508b4
anh.do <tuananh9h@gmail.com>
2024-08-05 11:18:59 +0700

    normalize.css classname init

---
+++ src/components/GlobalStyles/index.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GlobalStyles.scss';
+
+type GlobalStylesProps = {
+  children: React.ReactNode;
+};
+
+function GlobalStyles({ children }: GlobalStylesProps) {
+  return children;
+}
+
+export default GlobalStyles;
